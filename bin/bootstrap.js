5d6603369755a1c1230a79dd6fb90277eac165f3
saba <ssabripour@apple.com>
2014-08-13 18:25:04 -0700

    Updated bootstrap to use system port first

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -2,7 +2,7 @@ var app = require('../app');
 var config = require('config');
 
 var appConfig = config.get('app');
-app.set('port', appConfig.appPort || 3000);
+app.set('port', process.env.PORT || appConfig.appPort);
  
 var server = app.listen(app.get('port'), function() {
   console.log("I'm listening on port " + server.address().port);
